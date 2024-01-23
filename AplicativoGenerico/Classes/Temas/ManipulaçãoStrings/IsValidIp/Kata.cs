1898c62033a7c1c4e17eff574a049b04f5057b5c
diegobaladez <diegobaladez22@gmail.com>
2024-01-23 09:28:24 -0300

    Validador de IP com testes unitarios

---
+++ AplicativoGenerico/Classes/Temas/ManipulaçãoStrings/IsValidIp/Kata.cs
@@ -0,0 +1,49 @@
+﻿namespace AplicativoGenerico.Classes.Temas.ManipulaçãoStrings.IsValidIp
+
+{
+    public class Kata
+    {
+        public static bool is_valid_IP(string ipAddres)
+        {
+            int ipSections = 4;
+            string dotSeparator = ".";
+            
+            if (string.IsNullOrEmpty(ipAddres)) { return false; }
+
+            if(ipAddres.Any(Char.IsWhiteSpace)) { return false; }
+
+            string[] ipArray = ipAddres.Split(dotSeparator);
+
+            if(ipArray.Length != ipSections) return false;
+
+            foreach(var ipSection in ipArray)
+            {
+                if (!checkIpRange(ipSection)) return false;
+            }
+
+            return true;
+        }
+
+        private static bool checkIpRange(string ipSlice)
+        {
+            int ipUpperRange = 255;
+            int ipLowerRange = 0;
+
+            if (ipSlice.StartsWith("0") && ipSlice.Length > 1) return false;
+
+            try
+            {
+                if (int.Parse(ipSlice) < ipLowerRange || int.Parse(ipSlice) > ipUpperRange)
+                {
+                    return false;
+                }
+            }
+            catch(FormatException e)
+            {
+                return false;
+            }            
+
+            return true;
+        }
+    }
+}
--- AplicativoGenerico/Program.cs
+++ AplicativoGenerico/Program.cs
@@ -1,4 +1,4 @@
-﻿using AplicativoGenerico.Classes.Temas.ManipulaçãoStrings;
+﻿using AplicativoGenerico.Classes.Temas.ManipulaçãoStrings.IsValidIp;
 
 namespace AplicativoGenerico
 {
@@ -6,9 +6,22 @@ namespace AplicativoGenerico
     {
         static void Main(string[] args)
         {
-            string valor = "([)]";
+            string valor = "1.2.3.4";
 
-            Console.WriteLine(Brace.validBraces(valor));
+            List<string> invalidList = new List<string> {"1.2.3",
+                                                         "1.2.3.4.5",
+                                                         "123.456.78.90",
+                                                         "123.045.067.089",
+                                                         "abc.def.ghi.jkl",
+                                                         "pr12.34.56.78"
+                                                        };
+
+
+            foreach(string invalidIp in invalidList) 
+            {
+                Console.WriteLine(Kata.is_valid_IP(invalidIp));
+            }
+            
         }
     }
 }
